38964a3282ba1a3e5d176baa9e7eb57c1eafa9ce
vroobel95 <ada.wrobel95@gmail.com>
2026-02-17 11:58:11 +0100

    Adding users migration

--- WeddingPlan.Infrastructure/Persistence/Migrations/20260217105759_AddingUsers.Designer.cs
+++ WeddingPlan.Infrastructure/Persistence/Migrations/20260217105759_AddingUsers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeddingPlan.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using WeddingPlan.Infrastructure.Persistence;
 namespace WeddingPlan.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(WeddingPlannerDbContext))]
-    partial class WeddingPlannerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260217105759_AddingUsers")]
+    partial class AddingUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WeddingPlan.Infrastructure/Persistence/Migrations/20260217105759_AddingUsers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingPlan.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsEmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    ConfirmationToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
